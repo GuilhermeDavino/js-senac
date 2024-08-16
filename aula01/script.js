f5c303e3e4f76ee05dc38cbf25b06d6158ba1763
Guilherme de Albuquerque Davino <51931679+GuilhermeDavino@users.noreply.github.com>
2024-08-16 16:25:31 -0300

    primeira atividade

--- aula01/script.js
+++ aula01/script.js
@@ -1,4 +1,42 @@
-let a = 10;
-let b = 5;
-let sum = a+b;
-console.log(`Somas = ${sum}`);
+// Média aritmética
+
+let a = 12;
+let b = 6;
+let c = 3;
+let mediaAritmetica = (a+b+c)/3;
+console.log(`Média aritmética = ${mediaAritmetica}`);
+
+// Incremento
+
+let n1 = 1;
+n1++;
+console.log("Incremento: ", n1);
+
+// Resto da divisão
+
+let result = 10 % 3;
+console.log("Resto da divisão: ", result);
+
+// Farenheit para Celsius
+
+let f = 80;
+let celsius = (f - 32) / 1.8;
+
+console.log(f, "Fahrenheit = ", celsius, " Celsius");
+
+// dobro e metade de um numero
+
+let num2 = 2;
+let metade = 2/2;
+let dobro = 2*2;
+console.log("Dobro = ", dobro, " Metade = ", metade);
+
+// Exponenciação
+
+let exp = 2**5;
+
+console.log("2 elevado 5 = ", exp);
+
+
+
+
